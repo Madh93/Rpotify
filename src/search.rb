86690de6293d16b7ad2d597ec5c9eaa56f4df19e
Madh93 <michelangelo93@gmail.com>
2014-10-23 23:59:57 +0100

    Building search class

--- src/search.rb
+++ src/search.rb
@@ -1,1 +1,20 @@
 require_relative 'authentication'
+
+class Search
+
+	def initialize query
+		@query = query
+	end
+
+	def bySong
+		puts "song: " + @query
+	end
+
+	def byAlbum
+		puts "album: " + @query
+	end
+
+	def byArtist
+		puts "artist: " + @query
+	end
+end
