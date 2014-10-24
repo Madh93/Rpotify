501df4c50c0d92c4e8ef77928bdfaa85dc453641
Madh93 <michelangelo93@gmail.com>
2014-10-24 16:34:07 +0100

    Show results in a table

--- src/search.rb
+++ src/search.rb
@@ -1,4 +1,5 @@
 require_relative 'authentication'
+require 'terminal-table'
 
 class Search
 
@@ -7,7 +8,23 @@ class Search
 	end
 
 	def bySong
-		puts "song: " + @query
+
+		tracks = RSpotify::Base.search(@query, 'track')
+		rows = []
+
+		tracks.each_with_index do |t,i|
+
+			artist = t.artists[0].name
+			artist + "," + t.artists[1].name if t.artists.size > 1
+			min = t.duration_ms/60000
+			seg = (t.duration_ms/1000)%60
+			duration = min.to_s + "'" + seg.to_s + "''"
+				
+			rows << [i+1,t.name,artist,duration,t.popularity.to_s+"%",t.album.name]
+		end
+
+		 table = Terminal::Table.new :title => "Showing results for \e[1m#{@query}\e[0m", :headings => ["#","Song","Artist","Duration","Popularity","Album"], :rows => rows
+		 puts table
 	end
 
 	def byAlbum
