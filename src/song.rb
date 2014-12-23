700e5a1b5de1725e9a53fde2d75fff543a958118
Madh93 <michelangelo93@gmail.com>
2014-12-23 16:37:51 +0000

    defined Song information

--- src/song.rb
+++ src/song.rb
@@ -15,18 +15,27 @@ class Song
     private
 
     def setName
+        @metadata.name[0..40]
     end
 
     def setArtist
+        if @metadata.artists.size > 1
+            "#{@metadata.artists[0].name}, #{@metadata.artists[1].name}"[0..30]
+        else
+            @metadata.artists[0].name[0..30]
+        end     
     end
 
     def setAlbum
+        @metadata.album.name[0..30]
     end
 
     def setDuration
+        "#{@metadata.duration_ms/60000}' #{(@metadata.duration_ms/1000)%60}''"        
     end
 
     def setPopularity
+        "#{@metadata.popularity}"
     end
 
 end
